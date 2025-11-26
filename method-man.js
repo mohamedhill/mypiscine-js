17a83f2e38a308524868a6bd569628593f9b1a63
mhilli <zeroc7371@gmail.com>
2025-11-26 15:22:52 +0100

    method

---
+++ method-man.js
@@ -0,0 +1,6 @@
+const words = (sentence)=>sentence.split(" ")
+const sentence = (arr)=>arr.join(" ")
+const  yell = (str)=>str.toUpperCase()
+const whisper = (str)=>str.toLowerCase()
+const capitalize = (str)=>yell(str.charAt(0)) + whisper(str.slice(1));
+
