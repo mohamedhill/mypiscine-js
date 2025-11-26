9df8a8ec4ef2e2374bb17ed64bcbb166dc95e423
mhilli <zeroc7371@gmail.com>
2025-11-26 18:16:50 +0100

    is

--- is.js
+++ is.js
@@ -1,17 +1,12 @@
-const is = {}
-is.num = (arg) => (typeof arg === 'number')?true:false;
-is.str = (arg) => (typeof arg === 'string')?true:false;
-is.nan = (arg) => (typeof arg === 'NaN')?true:false;
-is.bool = (arg) => (typeof arg === 'boolean')?true:false;
-is.undef = (arg) => (typeof arg === 'undefined')?true:false;
-is.def = (arg) => (typeof arg !== 'undefined')?true:false;
-is.fun = (arg) => (typeof arg === 'function')?true:false;
+const is={}
+is.num = (arg) => typeof arg === 'number'
+is.str = (arg) => typeof arg === 'string'
+is.nan = (arg) => Number.isNaN(arg)
+is.bool = (arg) => typeof arg === 'boolean'
+is.undef = (arg) => typeof arg === 'undefined'
+is.def = (arg) => typeof arg !== 'undefined'
+is.fun = (arg) => typeof arg === 'function'
 is.obj = (arg) => (typeof arg === 'object' && !Array.isArray(arg) && arg !== null)?true:false;
-is.falsy = (arg) => (Boolean(arg) === false)?true:false;
-is.truthy = (arg) => (Boolean(arg) === true)?true:false;
+is.falsy = (arg) => !Boolean(arg) 
+is.truthy = (arg) => Boolean(arg)
 is.arr = (arg) => Array.isArray(value);
-
-let per={
-    test : 400
-}
-console.log(is.obj(per))
