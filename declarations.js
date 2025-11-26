3501a938c8d8e201f716511898e01edfaefed305
mhilli <zeroc7371@gmail.com>
2025-11-26 09:43:39 +0100

    declartions

--- declarations.js
+++ declarations.js
@@ -1,15 +1,16 @@
 let escapeStr = '`\/"\''
 
-let arr = [4,'2']
+const arr = [4,'2']
 
 let obj ={
     str : "hi",
     num : 10 ,
     bool : true,
+    undef : undefined,
     nested : {
         arr : [4,undefined,'2'],
         obj :{
-            str : "",
+            str : "hi",
             num : 10,
             bool : true
 
