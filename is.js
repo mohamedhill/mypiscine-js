585add00ba96a99bc0c68c674f7f45000152bdda
mhilli <zeroc7371@gmail.com>
2025-11-26 18:22:12 +0100

    is

--- is.js
+++ is.js
@@ -8,4 +8,4 @@ is.fun = (arg) => typeof arg === 'function'
 is.obj = (arg) => (typeof arg === 'object' && !Array.isArray(arg) && arg !== null)?true:false;
 is.falsy = (arg) => !Boolean(arg) 
 is.truthy = (arg) => Boolean(arg)
-is.arr = (arg) => Array.isArray(value);
+is.arr = (arg) => Array.isArray(arg);
