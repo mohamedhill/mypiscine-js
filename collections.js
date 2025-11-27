54b05c78d32630148c0477ebca940e0ce5bf5dd9
mhilli <zeroc3771@gmail.com>
2025-11-27 10:59:40 +0100

    collections

---
+++ collections.js
@@ -0,0 +1,40 @@
+
+const arrToSet = (arr)=>new Set(arr)
+const setToArray = (set)=>[...set]
+const arrToStr = (arr)=>arr.join("")
+const setToStr = (set)=>arrToStr(setToArray(set))
+const strToArr = (str)=>str.split("")
+const strToSet = (str)=>new set(str)
+const mapToObj = (maps)=>{Object.fromEntries(maps)}
+const objToArr = (obj)=>[Object.entries(obj)]
+const objTomap = (obj)=>new Map(Object.entries(obj))
+const arrToObj = (arr)=>{Object.fromEntries(arr)}
+const strToObj = (str)=>{JSON.parse(str)}
+
+const superTypeOf = function(item){
+    if (item===null){
+        return 'null'
+    }
+if (typeof item ==='object'){
+    if (Array.isArray(item)){
+        return 'Array'
+    }else if (item instanceof Map){
+        return 'Map'
+    }else if (item instanceof Set){
+        return 'Set'
+    }else{
+        return 'Object'
+    }
+}else if (typeof item==='string'){
+    return 'String'
+}else if(typeof item ==='undefined'){
+    return 'undefined'
+}else if (typeof item==='function'){
+    return 'Function'
+}else if (typeof item === 'number'){
+return 'Number'
+}else if (Number.isNaN(item)){
+    return 'Number'
+}
+
+}
