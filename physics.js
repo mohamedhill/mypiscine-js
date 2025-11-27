fb5b54098720105c2ba6b502fbb4bd782cc1d678
mhilli <zeroc3771@gmail.com>
2025-11-27 09:36:48 +0100

    physic

--- physics.js
+++ physics.js
@@ -1,4 +1,5 @@
 
 function getAcceleration(Formulas){
-return (Formulas.f/Formulas.m)||(Formulas.Δv/Formulas.Δt)||(2*Formulas.d/(Formulas.t*Formulas.t))
+return (Formulas.f/Formulas.m)||(Formulas.Δv/Formulas.Δt)||(2*Formulas.d/(Formulas.t*Formulas.t))||"impossible"
 }
+
