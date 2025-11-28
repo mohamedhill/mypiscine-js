6cc06b1b772bd6de578052588842f021143da037
mhilli <zeroc3771@gmail.com>
2025-11-28 17:20:29 +0100

    nasa

--- nasa.js
+++ nasa.js
@@ -2,15 +2,18 @@ function nasa(num){
 let res = ""
 for (let i  =1 ; i <= num;i++){
     if (i%3==0&&i%5==0){
-        res += "NASA "
+        res += "NASA"
     }else if (i%3==0){
-        res += "NA "
+        res += "NA"
     }else if (i%5==0){
-        res += "SA "
+        res += "SA"
     }else{
-        res += i+" "
+        res += i
     }
-
+if (i!==num){
+    res +=" "
+}
 }
 return res
 }
+console.log(nasa(15))
