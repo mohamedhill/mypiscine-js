c3667ccb65d6402ea9a2c1397c64d33c15ab3191
mhilli <zeroc3771@gmail.com>
2025-11-28 11:21:12 +0100

    index

--- index-of.js
+++ index-of.js
@@ -1,4 +1,8 @@
 function indexOf(arr,target,first){
+if (first === undefined){
+    first = 0
+}
+console.log(first)
 for (let i = first ; i< arr.length;i++){
     if (arr[i] ===target){
         return i
@@ -7,6 +11,9 @@ for (let i = first ; i< arr.length;i++){
 return -1
 }
 function lastIndexOf(arr,target,first){
+if (first === undefined){
+  first = 0
+}
 let res = []
 for (let i = first ; i< arr.length;i++){
     if (arr[i] ===target){
@@ -23,3 +30,4 @@ return -1
 }
 
 const includes = (arr,target)=>indexOf(arr,target)!==-1?true:false
+console.log(indexOf([1, 2, 3, 4, 5, 4, 3, 2, 1], 2))
