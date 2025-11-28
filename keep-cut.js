4f0ac20471e80d9d15dd7faca130999d54336958
mhilli <zeroc3771@gmail.com>
2025-11-28 13:34:28 +0100

    keep

--- keep-cut.js
+++ keep-cut.js
@@ -1,6 +1,6 @@
 const cutFirst = (str)=>str.slice(2)
 const cutLast = (str)=> str.slice(0,-2)
-const cutFirstLast = (str)=> str.slice(1,-2)
+const cutFirstLast = (str)=> str.slice(2,-2)
 const keepFirst = (str)=> str.slice(0,2)
 const keepLast = (str)=> str.slice(str.length-2)
 const keepFirstLast = function(str){
