2088e8532405fb0671557fd3fea76d88810290ec
mhilli <zeroc3771@gmail.com>
2025-11-28 14:49:44 +0100

    the last

--- reverser.js
+++ reverser.js
@@ -2,12 +2,14 @@ function reverse(arr){
     let res = []
 
 if (typeof arr ==='string'){
-for (let i = arr.length;i=>0;i++){
-res+=a[i]
+for (let i = arr.length-1;i>=0;i--){ 
+res+=arr[i]
 }
-}
-for (let i = arr.length;i=>0;i++){
-res.push(arr[i])
+}else{
+
+    for (let i = arr.length-1;i>=0;i--){
+    res.push(arr[i])
+    }
 }
 return res
 }
