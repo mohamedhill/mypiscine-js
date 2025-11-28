13dabe80e9322433be23b38a67f3bd6555c0b498
mhilli <zeroc3771@gmail.com>
2025-11-28 15:01:40 +0100

    the last

---
+++ unbreakable.js
@@ -0,0 +1,23 @@
+function split(str  ,spliter){
+    let res = []
+    let count = 0
+for (let i = 0 ; i <str.length;i++){
+if (str[i]===spliter){
+res.push(str.slice(count,i))
+count = i
+}
+}
+res.push(str.slice(count))
+return res
+}
+ function join(arr,joines){
+
+let res =""
+for (let i = 0 ; i<arr.length;i++){
+    res+= arr[i]
+    if (i != arr.length-1){
+        res+=joines
+    }
+}
+return res
+ }
