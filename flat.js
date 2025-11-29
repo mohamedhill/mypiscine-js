9a3e4ad6095dc9987df17ae1d29467496d5b370d
mhilli <howasimo57@gmail.com>
2025-11-29 14:21:30 +0100

    flat

--- flat.js
+++ flat.js
@@ -1,22 +1,25 @@
 function flat(arr,count=1){
 let res = []
+console.log(count)
 for (let item of arr){
     if (count >0){
     if (Array.isArray(item)){
+        console.log("item:",item,"res:",res)
         res=res.concat(flat(item,count-1))
-        count--
+        
     }else if (item !== undefined){
         res.push(item)
 
     }
 
-}else if (item !== undefined){
+} else if (item !== undefined){
      res.push(item)
 
-}
+} 
 }
 return res
 
 }
 
-console.log(flat([1, [2]]), [1, 2])
+console.log(flat([1, [2, [3], [4, [5]]]], 2)) 
+//[1, 2, 3, 4, [5]]
