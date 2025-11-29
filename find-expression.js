caf6cb086382a867407fc8acfe5d5ae2c4cc1db9
mhilli <howasimo57@gmail.com>
2025-11-29 15:39:13 +0100

    find-ex

--- find-expression.js
+++ find-expression.js
@@ -5,18 +5,18 @@ while (qu.length>0){
 
     let [current, seq] = qu.shift();
     if (current + 4=== num ){
-    return seq +' '+ '+4'
+    return seq +' '+ add4
 }else{
      
-qu.push([current+4,seq+' '+'+4'])
+qu.push([current+4,seq+' '+add4])
 
 }
 if (current * 2===num){
  
-    return seq + ' '+'*2'
+    return seq + ' '+mul2
 }else {
 
-  qu.push([current*2,seq+' '+'*2'])
+  qu.push([current*2,seq+' '+mul2])
 }
 
 }
