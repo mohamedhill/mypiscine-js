e382bfd3549a713dacb24ed71da183a21da10212
mhilli <howasimo57@gmail.com>
2025-12-04 16:19:12 +0100

    some time

--- get-some-time.js
+++ get-some-time.js
@@ -1,5 +1,23 @@
-function firstDayWeek(nb, year) {
-console.log(tests);
+function firstDayWeek(weekNumber, yearString) {
+  const year = parseInt(yearString);
+  const date = new Date(0);
+  date.setFullYear(year, 0, 1);
+  date.setHours(0, 0, 0, 0);
+  const dayOfWeek = date.getDay();
+  const daysToMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
+  date.setDate(date.getDate() - daysToMonday);
+  date.setDate(date.getDate() + (weekNumber - 1) * 7);
+  const firstJan = new Date(0);
+  firstJan.setFullYear(year, 0, 1);
+  firstJan.setHours(0, 0, 0, 0);
+  if (date.getTime() < firstJan.getTime()) {
+    date.setFullYear(year, 0, 1);
+  }
+  const dd = date.getDate();
+  const mm = date.getMonth() + 1;
+
+  
+  const yyyy = String(date.getFullYear()).padStart(4, "0");
+  return [dd < 10 ? "0" + dd : dd, mm < 10 ? "0" + mm : mm, yyyy].join("-");
 
 }
-firstDayWeek(10,2006)
