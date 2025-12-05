eb363023fecb54a2a0ad871c8d6ffe361a281e0c
mohamed hilli <mhilli@noreply.learn.zone01oujda.ma>
2025-12-05 10:51:12 +0000

    Update sunny-sunday.js

--- sunny-sunday.js
+++ sunny-sunday.js
@@ -1,14 +1,12 @@
 function sunnySunday(date){
+dif = new Date('0001-01-01')
 let days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 let miltoday = 60*60*24*1000
 date = new Date(date)
 
-let day = date /miltoday
-
-
-return days[day%6]
-
-
+let day = date -dif
+day = day / miltoday
+return days[day]
 
 
 
