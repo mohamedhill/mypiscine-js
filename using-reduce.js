5dfbf799fbed566b62205d2cb72996c19b4d90a0
mhilli <zeroc3771@gmail.com>
2025-12-05 17:04:31 +0100

    reduce

--- using-reduce.js
+++ using-reduce.js
@@ -5,8 +5,8 @@ function adder(arr,i=0){
 
 }
 
-function sumOrMul(arr){
-return arr.reduce((n,n1)=>n1%2===0?n*n1:n+n1)
+function sumOrMul(arr,i=0){
+return arr.reduce((n,n1)=>n1%2===0?n*n1:n+n1,i)
 }
 function funcExec(arr , x=0){
    
@@ -15,3 +15,4 @@ function funcExec(arr , x=0){
     
 
 }
+console.log(sumOrMul([18, 17, 7, 13, 25], 12));
