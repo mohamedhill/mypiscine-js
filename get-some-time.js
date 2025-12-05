55b005eaa1eb8958563a0fb4e1a6617d69f02b14
mhilli <howasimo57@gmail.com>
2025-12-05 09:51:59 +0100

    get-some-time

--- get-some-time.js
+++ get-some-time.js
@@ -1,23 +1,16 @@
-function firstDayWeek(weekNumber, yearString) {
-  const year = parseInt(yearString);
-  const date = new Date(0);
-  date.setFullYear(year, 0, 1);
-  date.setHours(0, 0, 0, 0);
-  const dayOfWeek = date.getDay();
-  const daysToMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
-  date.setDate(date.getDate() - daysToMonday);
-  date.setDate(date.getDate() + (weekNumber - 1) * 7);
-  const firstJan = new Date(0);
-  firstJan.setFullYear(year, 0, 1);
-  firstJan.setHours(0, 0, 0, 0);
-  if (date.getTime() < firstJan.getTime()) {
-    date.setFullYear(year, 0, 1);
+function firstDayWeek(nb, year) {
+  let d = new Date("01-01-0001");
+  d.setFullYear(year);
+  d.setDate(nb * 7 - 6);
+  while (d.getDate() !== 1 && d.getDay() !== 1) {
+    d.setDate(d.getDate() - 1);
   }
-  const dd = date.getDate();
-  const mm = date.getMonth() + 1;
 
-  
-  const yyyy = String(date.getFullYear()).padStart(4, "0");
-  return [dd < 10 ? "0" + dd : dd, mm < 10 ? "0" + mm : mm, yyyy].join("-");
-
+  return (
+    String(d.getDate()).padStart(2, "0") +
+    "-" +
+    String(d.getMonth() + 1).padStart(2, "0") +
+    "-" +
+    year
+  );
 }
