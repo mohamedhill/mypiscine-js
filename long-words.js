76a9cbb9518e80b90ba727ac2b92c94c8b2101e0
mhilli <zeroc3771@gmail.com>
2025-12-05 17:27:24 +0100

    long

---
+++ long-words.js
@@ -0,0 +1,12 @@
+function longWords(arr){
+return arr.every((n)=>typeof n === 'string'&&n.length>=5)
+}
+function oneLongWord(arr){
+return arr.some((n)=>typeof n === 'string'&& n.length>=10)
+
+
+}
+function noLongWords(arr){
+return !arr.every((n)=>typeof n === 'string'&&n.length>=7)
+
+}
