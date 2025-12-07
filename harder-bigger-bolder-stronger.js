85414addad007b1f66907d8ee92a2e4a8dd07e82
mhilli <howasimo57@gmail.com>
2025-12-07 11:06:02 +0100

    harder

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,22 @@
+export function generateLetters() {
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let fontsize = 11;
+  let fontweight = 300;
+  for (let i = 0; i < 120; i++) {
+    const n = Math.floor(Math.random() * 25) + 1;
+
+    let alp = alphabet[n];
+    const div = document.createElement("div");
+    div.textContent = alp;
+    div.style.fontSize = `${fontsize}px`;
+
+    div.style.fontWeight = fontweight;
+    document.body.appendChild(div);
+    fontsize++;
+    if (i === 40) {
+      fontweight = 400;
+    } else if (i === 80) {
+      fontweight = 600;
+    }
+  }
+}
