c6ccef1c66f2d096d48ed85b096f216755b5e3e8
mhilli <zeroc3771@gmail.com>
2025-12-09 17:24:36 +0100

    pick-omit

--- pick-omit.js
+++ pick-omit.js
@@ -3,7 +3,8 @@ let res = {}
 let keys = Object.keys(obj)
 if (typeof inc === 'string'){
     keys.forEach((key)=>{
-    if (inc.includes(key)){
+    if (inc ===key){
+        
         res[key]=obj[key]
     }
     })
@@ -17,6 +18,29 @@ if (typeof inc === 'string'){
 }
 return res
 }
+ function omit(obj,inc){
+    let res = {}
+let keys = Object.keys(obj)
+if (typeof inc === 'string'){
+    keys.forEach((key)=>{
+    if (inc !==key){
+        res[key]=obj[key]
+    }
+    })
 
 
-  
+}else{
+ keys.forEach((key)=>{
+    if (!inc.some((x)=>x.includes(key))){
+      
+        
+        res[key]=obj[key]
+    }
+    })
+    
+}
+return res
+ }
+
+ 
+ 
