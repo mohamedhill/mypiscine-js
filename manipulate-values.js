1941418644660cac5da59ceabed8dbb7a8669912
mhilli <howasimo57@gmail.com>
2025-12-10 17:10:08 +0100

    manipulate-values

---
+++ manipulate-values.js
@@ -0,0 +1,26 @@
+function filterValues(obj,callback){
+    let res = {}
+    for (let t in obj){
+        if (callback(obj[t])){
+            res[t]=obj[t]
+        }
+    }
+return res
+
+}
+
+function mapValues(obj,callback){
+let res = {}
+for (let t in obj){
+    res[t]= callback(obj[t])
+}
+return res
+}
+
+function reduceValues(obj,callback){
+    let acc = 0
+    for (let t in obj){
+        acc = callback(acc,obj[t])
+    }
+return acc
+}
