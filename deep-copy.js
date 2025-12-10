f0bf979ce61a280fc14b320abf6af179f870a45c
mhilli <zeroc3771@gmail.com>
2025-12-10 20:27:03 +0100

    deep

--- deep-copy.js
+++ deep-copy.js
@@ -1,14 +1,26 @@
 const deepCopy = (obj) => {
-
-  if (typeof obj !== "object" || obj === null) {
-    return obj;
-  }
-  const res = Array.isArray(obj) ? [] : {};
-
-  for (const key in obj) {
-    const value = obj[key];
-    res[key] = deepCopy(value);
-  }
-
-  return res;
-};
+    let res;
+    if(Array.isArray(obj)){
+        res = [];
+    } else {
+        res = {}
+    }
+    if (Array.isArray(obj)){
+        for(let i = 0 ; i < obj.length; i++){
+            if (Array.isArray(obj[i])){
+                res.push(deepCopy(obj[i]))
+            } else {
+                res.push(obj[i])
+            }
+        }
+    } else {
+        for (let key of Object.keys(obj)) {
+            if(typeof obj[key] === "object" && obj[key] !== null) {
+                res[key] = deepCopy(obj[key])
+            } else {
+                res[key] = obj[key]
+            }
+        }
+    }
+    return res;
+}
