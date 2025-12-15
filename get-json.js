58c85e089546a3e2403830165882c823becc3606
mhilli <zeroc3771@gmail.com>
2025-12-15 08:06:52 +0100

    get-json

--- get-json.js
+++ get-json.js
@@ -1,23 +1,15 @@
-function getJSON(){
-return async function(path,params) {
-    let searchParams
-    let url = path
-    if (params != undefined) {
-        searchParams = new URLSearchParams(params).toString()
-        url += "?" + searchParams
-    }
-    try {
-        const response = await fetch(url)
-        if (!response.ok) {
-            throw Error(response.statusText)
-        }
-        let json = await response.json()
-        if (json.error) {
-            throw Error(json.error)
-        }
-        return json.data
-    } catch (error) {
-        throw error
-    }
-}
-}
+async function getJSON(path, params = {}) {
+  let url =path
+  let searchParams =new URLSearchParams(params).toString()
+  if (searchParams) {
+    url += ?${searchParams}
+  }
+  let response = await fetch(url)
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+  let json = await response.json()
+  if (json.error) {
+    throw new Error(json.error)
+  }
+  return json.data
