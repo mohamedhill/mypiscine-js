a04465f4dea6e4e61cc0a03701b025b0754405b2
mhilli <zeroc3771@gmail.com>
2025-12-16 19:12:41 +0100

    verydisco

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,6 +1,6 @@
-let input = process.argv.slice(2)
+let input = process.argv[2]
 
-let splited = input.join(' ').split(' ')
+let splited = input.split(' ')
 
 let test = splited.map(word => {
 let word1 = word.slice(0,Math.ceil(word.length/2))
