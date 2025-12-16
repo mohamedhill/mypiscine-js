a780dfdb218d74a0c1e3a551bea9673eaf09de37
mhilli <zeroc3771@gmail.com>
2025-12-16 19:06:54 +0100

    verydisco

--- verydisco.mjs
+++ verydisco.mjs
@@ -5,7 +5,6 @@ let splited = input.join(' ').split(' ')
 let test = splited.map(word => {
 let word1 = word.slice(0,Math.ceil(word.length/2))
 let word2 = word.slice(Math.ceil(word.length/2))
-console.log(word1,word2);
 
 return word2+word1
     
